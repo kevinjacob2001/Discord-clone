c04f505a837f43c67ebe8f3c6073e83cf4efe81a
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-12-20 12:26:48 +0530

    unwanted files removed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import { Counter } from './features/counter/Counter';
+import { Counter } from './features/Counter';
 import './App.css';
+
+import Sidebar from './Components/Sidebar/Sidebar';
 
 function App() {
   return (
     <div className="app">
      <h1>Discord clone</h1>
+
+     {/* Sidebar */}
+     <Sidebar/>
     </div>
   );
 }
---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Sidebar=()=>{
+    return(
+        <div>
+            <h3>Sidebar component</h3>
+        </div>
+    )
+}
+
+
+export default Sidebar;
