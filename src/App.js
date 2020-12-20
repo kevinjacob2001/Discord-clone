e40dcf637031a0e88e384461c5f05449355cda4f
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-12-20 18:16:16 +0530

    addling authListener in app.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 
 import { useSelector } from "react-redux";
@@ -7,9 +7,24 @@ import { selectUser } from "./features/userSlice";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import Chat from "./Components/Chat/Chat";
 import Login from './Components/Login/Login'
+import { auth } from "./Components/firebase";
 
 function App() {
   const user=useSelector(selectUser)
+
+
+  useEffect(()=>{ 
+    auth.onAuthStateChanged((authUser)=>{
+      if(authUser){
+        //user is logged in
+      }
+      else{
+        //user is logged out
+      }
+    })
+
+  },[])
+
   return (
     <div className="app">
      
--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,12 +1,14 @@
 import { Button } from '@material-ui/core'
 import React from 'react'
 import './Login.css'
-import {auth} from '../firebase';
+import {auth,provider} from '../firebase';
 
 function Login(){
 
    const signIn=()=>{
-
+       auth.signInWithPopup(provider).catch((error)=>{
+alert(error.message)
+       })
    }
 
     return(
