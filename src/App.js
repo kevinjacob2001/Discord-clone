0da22f45a32d8395cfe15a739bac6a8f878b56ce
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-12-20 15:51:12 +0530

    adding login

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import Sidebar from './Components/Sidebar/Sidebar';
-import Chat from './Components/Chat/Chat'
+import Sidebar from "./Components/Sidebar/Sidebar";
+import Chat from "./Components/Chat/Chat";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
+
 
 function App() {
+  const user=useSelector(selectUser)
   return (
     <div className="app">
-    
+      {user?(
+        <>
+        <Sidebar />
 
-     {/* Sidebar */}
-     <Sidebar/>
-
-     {/* Chat */}
-     <Chat/>
-    
+      <Chat />
+        </>
+      ):(
+<h2>You need to login</h2>
+      )}
     </div>
-  );
+  ); 
 }
 
 export default App;
