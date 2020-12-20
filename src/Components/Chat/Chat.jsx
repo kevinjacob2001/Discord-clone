9591764f8cedc63e171a8baae7fbc2f1c4df421c
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-12-20 14:58:25 +0530

    addded layout

--- src/Components/Chat/Chat.jsx
+++ src/Components/Chat/Chat.jsx
@@ -8,6 +8,13 @@ function Chat(){
     return(
         <div className="chat">
             <ChatHeader/>
+
+            <div className="chat__message">
+
+            </div>
+            <div className="chat__input">
+                
+            </div>
         </div>
     )
 }
