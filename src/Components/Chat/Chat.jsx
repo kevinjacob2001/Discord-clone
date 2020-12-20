03909196746a1329e121a8421a72226cd7497fb2
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-12-20 15:04:08 +0530

    adding gif icons

--- src/Components/Chat/Chat.jsx
+++ src/Components/Chat/Chat.jsx
@@ -4,6 +4,13 @@ import './Chat.css'
 
 import ChatHeader from '../ChatHeader/ChatHeader';
 
+import AddCircleRoundedIcon from '@material-ui/icons/AddCircleRounded';
+import CardGiftcardIcon from '@material-ui/icons/CardGiftcard';
+import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
+import GifIcon from '@material-ui/icons/Gif';
+
+
+
 function Chat(){
     return(
         <div className="chat">
@@ -13,7 +20,17 @@ function Chat(){
 
             </div>
             <div className="chat__input">
-                
+                <AddCircleRoundedIcon fontSize="large"/>
+                <form>
+                    <input placeholder={`Message #TESTCHANNEL`}/>
+                    <button className="chat__inputButton" type="submit"> Send Message</button>
+                </form>
+                <div className="chat__inputIcons">
+                    <CardGiftcardIcon fontSize="large"/>
+                    <GifIcon fontSize="large"/>
+                    <EmojiEmotionsIcon fontSize="large"/>
+
+                </div>
             </div>
         </div>
     )
