a6fc79b53f4e3e2c87d0af343fa8a5b12df089fd
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-12-20 13:22:55 +0530

    styling channels header

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Sidebar.css'
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import AddIcon from '@material-ui/icons/Add';
 
 const Sidebar=()=>{
     return(
@@ -10,7 +11,13 @@ const Sidebar=()=>{
                 <ExpandMoreIcon/>
             </div>
             <div className="sidebar__channels">
-                
+                <div className="sidebar__channelsHeader">
+                    <div className="sidebar__header">
+                       <ExpandMoreIcon/>   
+                       <h4>Text Channels</h4>
+                    </div>
+                    <AddIcon className="sidebar__addChannel"/>
+                </div>
             </div>
         </div>
     )
