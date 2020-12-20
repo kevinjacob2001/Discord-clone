cdd278bc2c44b9f641cfb78ca044d15d77cbaf2f
kevinjacob2001 <kevinjacob2001@gmail.com>
2020-12-20 13:38:31 +0530

    adding dummy list

---
+++ src/Components/Sidebar_Channel/SidebarChannel.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './SidebarChannel.css'
+
+const SidebarChannel=({id,channel})=>{
+    return(
+        <div>
+              <h4><span className="sidebarChannel__hash">#</span>KevinJacob</h4>
+        </div>
+    )
+}
+
+
+export default SidebarChannel;
